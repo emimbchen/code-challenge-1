4fbe063ad8b907fa70bb06e9f723337b69f86c85
Emi Chen <emimbchen@gmail.com>
2017-10-09 14:12:13 -0500

    have swap button functioning

--- client.js
+++ client.js
@@ -6,6 +6,8 @@ $(document).ready(goQuery);
 function goQuery(){
   console.log('query went');
   $('button').on('click', buttonClicked);
+  $('#divplace').on('click', '#swap', swapButton);
+  $('#divplace').on('click', '#delete', deleteButton);
 }
 //universal variable
 var buttonCounter = 0;
@@ -13,4 +15,13 @@ var buttonCounter = 0;
 function buttonClicked(){
   buttonCounter += 1;
   $('#divplace').append('<div><p>'+ buttonCounter + '</br> </br><button id="swap">Swap</button><button id="delete">Delete</button></p></div>');
+}
+
+function swapButton(e){
+  var $targetDiv = $(e.target).parent();
+$($targetDiv).toggleClass('yellow');
+}
+
+function deleteButton(){
+
 }
